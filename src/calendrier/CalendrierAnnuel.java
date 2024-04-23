6606b53cd95871845efccc5439332539db821693
ksamyn31 <ksamyn31@gmail.com>
2024-04-23 17:35:03 +0200

    jsp quoi faire

--- src/calendrier/CalendrierAnnuel.java
+++ src/calendrier/CalendrierAnnuel.java
@@ -1,41 +1,48 @@
 package calendrier;
 
+
 public class CalendrierAnnuel {
-	CalendrierAnnuel[] calendrier;
-	public CalendrierAnnuel() {
-		CalendrierAnnuel calendrier = new CalendrierAnnuel();
-			mois("Janvier",31);
-			mois("Fevrier",28);
-			mois("Mars",31);
-			mois("Avril",30);
-			mois("Mai",31);
-			mois("Juin",30);
-			mois("Juillet",31);
-			mois("Ao�t",31);
-			mois("Septembre",30);
-			mois("Octobre",31);
-			mois("Novembre",30);
-			mois("D�cembre",31);
-			
-	}
-	
 	
 	public class Mois {
-		String nom;
-		boolean jour;
-		
-		public Mois(String nom, int nbJours) {
-			
-		}
-		
-		private boolean estLibre(int jour) {
-			return true;
-		}
-		
-		private void reserver(int jour) {
-			
-		}
-	
-	
+	    private String nom;
+	    private boolean[] jours;
+
+	    public Mois(String nom, int nombreJours) {
+	        this.nom = nom;
+	        this.jours = new boolean[nombreJours];
+	    }
+
+	    public boolean estLibre(int jour) {
+	        return !jours[jour - 1];
+	    }
+
+	    public void reserver(int jour) {
+	        if (!estLibre(jour - 1)) {
+	            throw new IllegalStateException("Ce jour est déjà réservé.");
+	        }
+	        jours[jour - 1] = true;
+	    }
 	}
+
+	public class Calendrier {
+	    private Mois[] calendrier;
+
+	    public Calendrier() {
+	        calendrier = new Mois[12];
+	        calendrier[0] = new Mois("Janvier", 31);
+	        calendrier[1] = new Mois("Février", 28);
+	        calendrier[2] = new Mois("Mars", 31);
+	        calendrier[3] = new Mois("Avril", 30);
+	        calendrier[4] = new Mois("Mai", 31);
+	        calendrier[5] = new Mois("Juin", 30);
+	        calendrier[6] = new Mois("Juillet", 31);
+	        calendrier[7] = new Mois("Août", 31);
+	        calendrier[8] = new Mois("Septembre", 30);
+	        calendrier[9] = new Mois("Octobre", 31);
+	        calendrier[10] = new Mois("Novembre", 30);
+	        calendrier[11] = new Mois("Décembre", 31);
+	    }
+	    
+	}
+
 }
